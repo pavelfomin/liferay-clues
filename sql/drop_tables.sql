7f6e5890e6df625257d40405973c7c71f1b158b4
Pavel Fomin <p.v.fomin@gmail.com>
2015-01-28 12:39:36 -0600

    Added drop tables script.

---
+++ sql/drop_tables.sql
@@ -0,0 +1,7 @@
+-- Start with a fresh db by deleting the tables
+-- If images are stored on the filesystem then that directory needs to be cleaned up as well.
+-- Start one node of Liferay to make sure that the tables creation is performed correctly. 
+select 
+	'drop table '||table_name ||';' 
+  from user_tables
+;
